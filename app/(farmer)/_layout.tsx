de79a9b85062a1198946d11082c1c50ea374dc85
Rahul Prasad <rahulprasad5943@gmail.com>
2026-05-14 23:29:01 +0530

    hh

--- app/(farmer)/_layout.tsx
+++ app/(farmer)/_layout.tsx
@@ -4,13 +4,12 @@ import { BottomTabs } from '../../components/ui/BottomTabs';
 export default function FarmerLayout() {
   return (
     <Tabs
-      tabBar={(props) => <BottomTabs {...props} />}
+      tabBar={(props) => <BottomTabs {...props} hiddenTabs={['reports']} />}
       screenOptions={{ headerShown: false }}
     >
       <Tabs.Screen name="dashboard" />
       <Tabs.Screen name="farms" />
       <Tabs.Screen name="tasks" />
-      <Tabs.Screen name="reports" />
       <Tabs.Screen name="profile" />
       <Tabs.Screen name="notifications" options={{ href: null }} />
     </Tabs>
