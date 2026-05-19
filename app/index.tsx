a341ad67a00659195882247389479dd109cba13c
rahulprasad5080 <rahulprasad5080@gmail.com>
2026-05-19 18:10:27 +0530

    fgf

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { Redirect } from "expo-router";
 
+// This is the entry point of the app. It redirects to the login screen.
+
 export default function Index() {
   return <Redirect href="/(auth)/login1" />;
 }
